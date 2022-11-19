ae230bc86b1bc382c484eb4a40c8da8009d0bb93
jim <jimorcheson@gmail.com>
2022-11-19 12:43:47 -0500

    Add deleted copy/move constructors and operator=

--- src/jimo/include/StopWatch.h
+++ src/jimo/include/StopWatch.h
@@ -1,8 +1,35 @@
+///
+/// \file StopWatch.h
+///
+/// \namespace jimo
+/// \brief The main namespace for all classes in this library.
+/// \namespace timing
+/// \brief The namespace for all classes related to timing, such as
+/// StopWatch and Timer.
 namespace jimo::timing
 {
+    /// \class StopWatch
+    /// \brief A stopwatch class
+    ///
+    /// This class provides methods for timing activities and includes a lap
+    /// timer.
     class StopWatch
     {
         public:
+            /// \name Constructors
+            /// The constructors for the StopWatch class.
+            ///@{
+
+            /// \brief The default StopWatch constructor
             StopWatch();
+            /// \brief Copy constructor
+            StopWatch(const StopWatch&) = delete;
+            /// \brief Move constructor
+            StopWatch(StopWatch&&) = delete;
+            /// \brief Copy operator=
+            StopWatch& operator =(const StopWatch&) = delete;
+            /// \brief Move operator=
+            StopWatch& operator =(StopWatch&&) = delete;
+            ///@}
     };
 }
