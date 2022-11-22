6d13f72e3f3aa5acdb7075349c10841818f9a123
jim <jimorcheson@gmail.com>
2022-11-22 08:46:58 -0500

    Update StopWatch documentation

--- src/jimo/include/StopWatch.h
+++ src/jimo/include/StopWatch.h
@@ -20,22 +20,7 @@ namespace jimo::timing
     ///
     /// <A HREF="https://en.cppreference.com/w/cpp/chrono/steady_clock">
     /// std::chrono::steady_clock</A> is used internally in this class. 
-    /// Using <A HREF="https://en.cppreference.com/w/cpp/chrono/system_clock">
-    /// std::chrono::system_class</A> would cause problems around time change
-    /// times (e.g. when switching from daylight savings time to standard time),
-    /// resulting in possibly incorrect durations and lap times.
-    ///
-    /// <A HREF="https://en.cppreference.com/w/cpp/chrono/high_resolution_clock">
-    /// std::chrono::high_resolution_clock</A> is not used because it is not implemented
-    /// consistently across different standard library implementations. It is often
-    /// just an alias for system_time or steady_time. For example, for gcc's libstdc++
-    /// it is system_clock, for MSVC it is steady_clock, and for clang's libc++ it
-    /// depends on configuration.
-    /// \tparam duration_t An <A HREF="https://en.cppreference.com/w/cpp/chrono/duration">
-    /// std::chrono::duration</A> type. You should consider
-    /// using one of the convenience duration types, such as 
-    /// <A HREF="https://en.cppreference.com/w/cpp/chrono/duration">std::chrono::nanoseconds,
-    /// std::chrono::milliseconds, std::chrono::minutes</A>, and so forth.
+
     class StopWatch
     {
         public:
@@ -65,9 +50,11 @@ namespace jimo::timing
             /// \returns A vector of lap times.
             std::vector<std::chrono::nanoseconds> GetLapTimes();
             /// \brief Start timing
-            /// \throws StopWatchException if you call Start() when the watch is already running.
+            /// \exception StopWatchException if you call Start() when the watch is already running.
             void Start();
             /// \brief Save timing of current lap and start timing of next lap
+            /// \exception StopWatchException if you call StartNextLap() when the watch is
+            /// not running.
             void StartNextLap();
             /// \brief Stop timing
             /// \exception StopWatchException if you attempt to stop a stop watch that
