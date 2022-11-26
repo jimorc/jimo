456c15ac2e14b0ac2062e3d7fbed775f960b1175
jim <jimorcheson@gmail.com>
2022-11-26 06:36:17 -0500

    Update StopWatch example to throw exception

--- examples/StopWatch/StopWatch.cpp
+++ examples/StopWatch/StopWatch.cpp
@@ -8,27 +8,36 @@ using namespace std::chrono_literals;
 
 int main()
 {
-    StopWatch watch;
-    watch.Start();
-    // do something that you want to time
-    std::this_thread::sleep_for(500us);
-    watch.Stop();
-    auto duration = watch.GetDuration();
-    std::cout << "Task took " << duration.count() << "ns\n";
+    try
+    {
+        StopWatch watch;
+        watch.Start();
+        // do something that you want to time
+        std::this_thread::sleep_for(500us);
+        watch.Stop();
+        auto duration = watch.GetDuration();
+        std::cout << "Task took " << duration.count() << "ns\n";
 
-    watch.Start();
-    // do something that you want to time laps for
-    for (int i = 0; i < 10; ++i)
-    {
-        std::this_thread::sleep_for(200us);
-        watch.StartNextLap();
+        watch.Start();
+        // do something that you want to time laps for
+        for (int i = 0; i < 10; ++i)
+        {
+            std::this_thread::sleep_for(200us);
+            watch.StartNextLap();
+        }
+        watch.StopWithoutSavingTime();
+        auto durations = watch.GetLapTimes();
+        std::cout << "Here are the " << durations.size() << " lap times:\n";
+        for (auto dur : durations)
+        {
+            auto lapTime = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
+            std::cout << "    " << lapTime << "us\n";
+        }
+        // this throws exception
+        watch.Stop();
     }
-    watch.StopWithoutSavingTime();
-    auto durations = watch.GetLapTimes();
-    std::cout << "Here are the " << durations.size() << " lap times:\n";
-    for (auto dur : durations)
+    catch (StopWatchException& e)
     {
-        auto lapTime = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
-        std::cout << "    " << lapTime << "us\n";
+        std::cout << e.what() << std::endl;
     }
 }
