30d6750cf916b053cc2faa5afc867c76cb38f7c8
Jim Orcheson <jimorcheson@gmail.com>
2023-02-01 10:36:36 -0500

    Add constructors, operator = methods

--- src/jimo/include/Action.h
+++ src/jimo/include/Action.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <any>
+#include <type_traits>
 #include "Delegate.h"
 
 namespace jimo::threading
@@ -21,6 +22,48 @@ namespace jimo::threading
     requires std::is_enum_v<actionEnum_t>
     struct Action
     {
+        /// @brief Default constructor
+        Action() = default;
+        /// @brief Copy constructor
+        /// @param other The Action object to copy.
+        Action(const Action& other) noexcept
+            : action(other.action), actionData(other.actionData), 
+                actionCallback(other.actionCallback) {}
+        /// @brief Move constructor
+        /// @param other The Action object to move.
+        Action(Action&& other) noexcept
+            : action(std::move(other.action)), actionData(std::move(other.actionData)),
+                actionCallback(std::move(other.actionCallback)) 
+        {
+            other.action = {};
+            other.actionData = {};
+            other.actionCallback.clear();
+        }
+        /// @brief Destructor
+        virtual ~Action() = default;
+        /// @brief Copy equals operator
+        /// @param other The Action object to copy.
+        /// @return The copied Action object.
+        Action& operator =(const Action& other) noexcept
+        {
+            action = other.action;
+            actionData = other.actionData;
+            actionCallback = other.actionCallback;
+            return *this;
+        };
+        /// @brief Move equals operator
+        /// @param other The Action object to move.
+        /// @return The moved Action object.
+        Action& operator =(Action&& other) noexcept
+        {
+            action = other.action;
+            actionData = other.actionData;
+            actionCallback = other.actionCallback;
+            other.action = {};
+            other.actionData = {};
+            other.actionCallback.clear();
+            return *this;
+        }
         /// @brief The action that the ActionHandler or ThreadActionHandler is to perform.
         /// @note The action is defined in the actionHandler or ThreadActionHandler, not
         /// in the `Action` struct. Potentially, an action could be specified in the
@@ -30,6 +73,8 @@ namespace jimo::threading
         actionEnum_t action;
         /// @brief Data to be passed to the method in the ActionHandler or
         /// ThreadActionHandler.
+        /// @note The actionData type must be copyable and movable. If not, then a
+        /// compilation error will be generated when attempting to set this value.
         std::any actionData {};
         /// @brief Any callbacks that are to be called after the action is completed.
         jimo::Delegate<void, std::any> actionCallback;
--- tests/ActionTests.cpp
+++ tests/ActionTests.cpp
@@ -2,24 +2,89 @@
 /// @author Jim Orcheson
 /// @copyright 2023 Jim Orcheson. Use dictated by MIT License.
 
+#include <vector>
+#include <memory>
+#include <utility>
 #include <gtest/gtest.h>
 #include "Action.h"
 
 using namespace jimo;
 using namespace jimo::threading;
 
+enum class Actions
+{
+    first,
+    last,
+};
+
 TEST(ActionTests, testNoArgsConstructor)
 {
-    enum class Actions
-    {
-        first,
-        last,
-    };
-    int value {};
+    int value{};
     Action<Actions> action;
     action.action = Actions::first;
     action.actionData = 4;
-    action.actionCallback += [&value](std::any val){ value = std::any_cast<int>(val); };
+    action.actionCallback += [&value](std::any val)
+    { value = std::any_cast<int>(val); };
     action.actionCallback(3);
     ASSERT_EQ(3, value);
+}
+
+TEST(ActionTests, testCopyConstructor)
+{
+    auto func = [](const std::any &) { /* do nothing */ };
+    Action<Actions> action;
+    action.action = Actions::last;
+    action.actionData = 42;
+    action.actionCallback += func;
+    Action action2(action);
+    ASSERT_EQ(action.action, action2.action);
+    ASSERT_EQ(std::any_cast<int>(action.actionData), std::any_cast<int>(action2.actionData));
+    ASSERT_EQ(action.actionCallback, action2.actionCallback);
+}
+
+TEST(ActionTests, testMoveConstructor)
+{
+    auto func = [](const std::any &) { /* do nothing */ };
+    Action<Actions> action;
+    action.action = Actions::last;
+    action.actionData = 42;
+    action.actionCallback += func;
+    Action<Actions> actionCopy(action);
+    Action action2(std::move(action));
+    ASSERT_EQ(Actions::last, action2.action);
+    ASSERT_EQ(42, std::any_cast<int>(action2.actionData));
+    ASSERT_EQ(actionCopy.actionCallback, action2.actionCallback);
+    ASSERT_EQ(Actions::first, action.action);
+    ASSERT_FALSE(action.actionData.has_value());
+    ASSERT_TRUE(action.actionCallback.empty());
+}
+
+TEST(ActionTests, testCopyEquals)
+{
+    auto func = [](const std::any &) { /* do nothing */ };
+    Action<Actions> action;
+    action.action = Actions::last;
+    action.actionData = 42;
+    action.actionCallback += func;
+    Action<Actions> action2 = action;
+    ASSERT_EQ(action.action, action2.action);
+    ASSERT_EQ(std::any_cast<int>(action.actionData), std::any_cast<int>(action2.actionData));
+    ASSERT_EQ(action.actionCallback, action2.actionCallback);
+}
+
+TEST(ActionTests, testMoveEquals)
+{
+    auto func = [](const std::any &) { /* do nothing */ };
+    Action<Actions> action;
+    action.action = Actions::last;
+    action.actionData = 42;
+    action.actionCallback += func;
+    Action<Actions> actionCopy(action);
+    Action<Actions> action2 = std::move(action);
+    ASSERT_EQ(Actions::last, action2.action);
+    ASSERT_EQ(42, std::any_cast<int>(action2.actionData));
+    ASSERT_EQ(actionCopy.actionCallback, action2.actionCallback);
+    ASSERT_EQ(Actions::first, action.action);
+    ASSERT_FALSE(action.actionData.has_value());
+    ASSERT_TRUE(action.actionCallback.empty());
 }
